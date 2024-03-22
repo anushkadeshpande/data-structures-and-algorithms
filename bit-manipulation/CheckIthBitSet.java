2f24ca5522baedba14f0f142770dcd9b14059c37
anushkadeshpande <anushkaa5000@gmail.com>
2024-03-23 00:58:19 +0530

    Added code to check if ith bit is set using right shift operator

--- bit-manipulation/CheckIthBitSet.java
+++ bit-manipulation/CheckIthBitSet.java
@@ -3,12 +3,21 @@ public class CheckIthBitSet {
     // Using left shift operator
 
     int num = 4;
-    int i = 2;
+    int i = 1;
 
     if((num & (1 << i)) != 0) {
       System.out.println("Bit is set");
     } else {
       System.out.println("Bit is not set");
     }
+
+
+    // Using right shift operator
+    if(((num >> i) & 1) == 1) {
+      System.out.println("Bit is set");
+    } else {
+      System.out.println("Bit is not set");
+    }
+
   }  
 }

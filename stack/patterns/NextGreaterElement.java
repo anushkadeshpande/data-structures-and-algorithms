103d3107e06a1391888b37909fcf37f537166400
anushkadeshpande <anushkaa5000@gmail.com>
2024-04-20 18:48:42 +0530

    Added stack question pattern- NextGreaterElement

---
+++ stack/patterns/NextGreaterElement.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+
+public class NextGreaterElement {
+    /* 
+     * Steps:
+     * 1. Traverse the array from last to first
+     * 2. If stack is not empty, and while stack.top() < arr[i], pop from the stack
+     * 3. Now, if stack is empty, put -1 in result array, else whatever is at the top is the next greatest element
+     * 4. Push the current element in the stack
+     */
+  public static long[] nextLargerElement(long[] arr, int n)
+    { 
+        Stack<Long> stack = new Stack<>();
+        long[] answer = new long[n];
+        for(int i=n-1; i>=0; i--) {
+                while(!stack.empty() && stack.peek() <= arr[i])
+                    stack.pop();
+                    
+                if(stack.empty())
+                    answer[i] = -1;
+                else
+                    answer[i] = stack.peek();
+                stack.push(arr[i]);
+            
+        }
+        
+        return answer;
+    } 
+    
+    public static void main (String[] args) {
+          long[] arr = {1, 3, 2, 4};
+          long[] res = nextLargerElement(arr, arr.length);
+          for(int i=0; i<arr.length; i++)
+            System.out.println(res[i]);
+    }
+}

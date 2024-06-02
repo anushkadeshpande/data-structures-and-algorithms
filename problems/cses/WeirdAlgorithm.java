ee9f6e83f985edba159a47d950bce9fa4e0f6877
anushkadeshpande <anushkaa5000@gmail.com>
2024-06-02 21:56:22 +0530

    cses: 001 - Weird Algorithm

---
+++ problems/cses/WeirdAlgorithm.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class WeirdAlgorithm {
+
+  public static void weirdAlgorithm(long n) {
+    System.out.print(n + " ");
+
+    if (n == 1)
+      return;
+
+    if (n % 2 == 0)
+      n /= 2;
+    else
+      n = (n * 3) + 1;
+
+    weirdAlgorithm(n);
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    long n = sc.nextInt();
+
+    sc.close();
+
+    weirdAlgorithm(n);
+  }
+}

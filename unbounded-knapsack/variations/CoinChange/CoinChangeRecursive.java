a64d418ded9d3aa89bdc89c3bf7fa1e4dcf4ed5f
anushkadeshpande <anushkaa5000@gmail.com>
2024-07-03 19:45:28 +0530

    Added Recursive solution to coin change problem

---
+++ unbounded-knapsack/variations/CoinChange/CoinChangeRecursive.java
@@ -0,0 +1,19 @@
+public class CoinChangeRecursive {
+
+  public static long count(int coins[], int N, int sum) {
+    if (sum == 0)
+      return 1;
+
+    if (N <= 0)
+      return 0;
+
+    if (coins[N - 1] <= sum)
+      return (count(coins, N, sum - coins[N - 1]) + count(coins, N - 1, sum));
+    else
+      return count(coins, N - 1, sum);
+  }
+
+  public static void main(String[] args) {
+    System.out.println(count(new int[] { 1, 2, 3 }, 3, 4));
+  }
+}

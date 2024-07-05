8019d7bb05da2e5a15ed451699637762d92e2f5e
anushkadeshpande <anushkaa5000@gmail.com>
2024-07-06 03:26:14 +0530

    Added implementation for trie

---
+++ trie/Trie.java
@@ -0,0 +1,83 @@
+class Node {
+  Node[] links = new Node[26];
+  boolean flag = false;
+
+  public boolean containsKey(char ch) {
+    return links[ch - 'a'] != null;
+  }
+
+  public void put(char ch) {
+    this.links[ch - 'a'] = new Node();
+  }
+
+  public Node get(char ch) {
+    return this.links[ch - 'a'];
+  }
+
+  public void setEnd() {
+    this.flag = true;
+  }
+
+  public boolean isEnd() {
+    return this.flag;
+  }
+}
+
+public class Trie {
+  private Node root;
+
+  Trie() {
+    root = new Node();
+  }
+
+  // TC: O(word.length())
+  public void insert(String word) {
+    Node temp = this.root;
+    int i = 0;
+    while (i < word.length()) {
+      // if the current char does not exist in trie, add it and create a new node at
+      // that position
+      if (!temp.containsKey(word.charAt(i))) {
+        // temp.links[word.charAt(i) - 'a'] = new Node();
+        temp.put(word.charAt(i));
+      }
+
+      // move to the reference trie
+      temp = temp.get(word.charAt(i));
+
+      i++;
+    }
+
+    temp.setEnd();
+  }
+
+  public boolean search(String word) {
+    Node temp = this.root;
+    int i = 0;
+
+    while (i < word.length()) {
+      if (!temp.containsKey(word.charAt(i)))
+        return false;
+
+      temp = temp.get(word.charAt(i));
+      i++;
+    }
+
+    return temp.isEnd();
+  }
+
+  public boolean startsWith(String prefix) {
+    Node temp = this.root;
+    int i = 0;
+
+    while (i < prefix.length()) {
+      if (!temp.containsKey(prefix.charAt(i)))
+        return false;
+
+      temp = temp.get(prefix.charAt(i));
+      i++;
+    }
+
+    return true;
+  }
+}

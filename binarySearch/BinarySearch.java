e31cabb506dbb6d6af68f3a0e797d7e0cd9f2379
anushkadeshpande <anushkaa5000@gmail.com>
2024-11-14 02:45:23 +0530

    Added Iterative code for binary search

---
+++ binarySearch/BinarySearch.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class BinarySearch {
+  public static int search(int arr[], int num) {
+    Arrays.sort(arr);
+
+    int start = 0;
+    int end = arr.length - 1;
+
+    while (start < end) {
+      int mid = (start + end) / 2;
+
+      if (arr[mid] == num)
+        return mid;
+
+      else if (arr[mid] > num)
+        end = mid - 1;
+      else
+        start = mid + 1;
+    }
+
+    return -1;
+  }
+
+  public static void main(String[] args) {
+    int[] arr = new int[] { 7, 4, 8, 2, 5, 1, 9, 3, 6 };
+    System.out.println(search(arr, 5));
+    System.out.println(search(arr, 3));
+    System.out.println(search(arr, 0));
+    System.out.println(search(arr, 100));
+  }
+}
